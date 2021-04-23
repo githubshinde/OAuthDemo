6d8e4fd4f82570033ed5c9f5c5cb144c58ff73cf
githubshinde <48369922+githubshinde@users.noreply.github.com>
2021-04-23 19:29:54 +0100

    Initial commit

---
+++ src/main/java/OAuthDemo.java
@@ -0,0 +1,23 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.given;
+
+public class OAuthDemo {
+    public static void main(String[] args) {
+
+     String accessTokenResponse = given().
+              queryParams("code","").
+              queryParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com").
+              queryParams("client_secret","erZOWM9g3UtwNRj340YYaK_W").
+              queryParams("redirect_uri","https://rahulshettyacademy.com/getCourse.php").
+              queryParams("grant_type","authorization_code").
+              when().
+              post("https://www.googleapis.com/oauth2/v4/token").asString();
+
+        JsonPath js = new JsonPath(accessTokenResponse);
+        String accessToken = js.getString("access_token");
+
+
+    }
+}
